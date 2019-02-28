4abf8ae4e8e9ab93dd9a34b964002f9daad5d9f2
Erny Nursetyawati <ernynursetyawati98@gmail.com>
2019-02-28 14:53:04 +0700

    init commit

---
+++ app/src/main/java/com/example/healthhappy/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.example.healthhappy;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent theIntent =  new Intent(SplashScreen.this, MainActivity.class);
+                SplashScreen.this.startActivity(theIntent);
+                SplashScreen.this.finish();
+            }
+        },3000);
+
+
+    }
+}
