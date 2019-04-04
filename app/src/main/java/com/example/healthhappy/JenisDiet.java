8b21b57335e8d5aaaeef7651460b463cf4fd8545
Annisa Dwiayu Ramadhanty <annisadwiayu@gmail.com>
2019-04-04 14:22:05 +0700

    'menambahkan'

---
+++ app/src/main/java/com/example/healthhappy/JenisDiet.java
@@ -0,0 +1,14 @@
+package com.example.healthhappy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JenisDiet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jenis_diet);
+    }
+}
+
