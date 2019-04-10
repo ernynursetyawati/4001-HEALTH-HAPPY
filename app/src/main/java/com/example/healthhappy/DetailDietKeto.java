365b4f27a582ff75c34ce31ed7f43e7ab0c61dee
Erny Nursetyawati <ernynursetyawati98@gmail.com>
2019-04-10 21:04:45 +0700

    'tambah'

---
+++ app/src/main/java/com/example/healthhappy/DetailDietKeto.java
@@ -0,0 +1,13 @@
+package com.example.healthhappy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailDietKeto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_diet_keto);
+    }
+}
